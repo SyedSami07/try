a8d41988781b90683fcf34748b5a5c438651cee7
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-19 00:15:17 +0600

    continue in loops

---
+++ JavaScript/continue.js
@@ -0,0 +1,20 @@
+// for(let i = 0; i <=10; i++)
+// {
+//     if(i==6){
+//         continue;
+//     }
+//     console.log("value of i: ", i)
+// }
+
+// with while loop
+
+let i = 0;
+while(i<=10){
+   if(i==6){
+    i++;
+        continue;
+    }
+    console.log("value of i: ", i);
+     i++;
+   
+}
