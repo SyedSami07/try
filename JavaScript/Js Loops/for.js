0eb2c7ae60b5e1e565066f1b7f76c6abdade3638
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-18 23:35:33 +0600

    Update for.js

---
+++ JavaScript/Js Loops/for.js
@@ -0,0 +1,15 @@
+for (let index = 0; index < 10; index++) {
+    console.log('value of i', index)
+
+}
+
+
+for( let n = 0; n < 7; n++){
+    console.log('sami`s roll number is: ', n)
+}
+
+let s = 0;
+while(s<32){
+    console.log('sami only goes to: ', s)
+    s++;
+}
