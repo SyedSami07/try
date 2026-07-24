7432b50928ff30a80604078c63647133d6f18092
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-24 16:45:32 +0600

    Add files via upload

---
+++ JavaScript/Js String/bracket.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'syed sami',
+    profession: 'student',
+    age: 25,
+    isSingle: true,
+    'home address': 'sylhet'
+}
+
+// console.log(person['profession'])
+console.log(person['home address'])
---
+++ JavaScript/Js String/dot.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'syed sami',
+    profession: 'student',
+    age: 25,
+    isSingle: true
+}
+
+console.log(person.profession)
