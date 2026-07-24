6d5bb455cf0612bb5d84933fbdcfda4ff8769dcd
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-24 15:36:21 +0600

    introduction to js strings

---
+++ JavaScript/Js String/string.js
@@ -0,0 +1,23 @@
+const cup = 'world cup';
+const tea = 'tea cup';
+const location = `Bandor bon`;
+// const vehicle = new String ('car');
+
+
+const numbers = [4,54,6,78,11];
+console.log(numbers.length);
+
+console.log(tea.length);
+
+console.log(numbers[0]);
+console.log(location[0]);
+
+numbers[0] =  99;
+console.log(numbers);
+console.log(numbers[0]);
+
+// console.log(location)
+
+location[0] = 'F';
+
+console.log(location);
