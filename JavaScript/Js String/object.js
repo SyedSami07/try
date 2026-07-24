1ae7dbe33759bd15482038d3a15b5633b1b98283
Syed Salman Sami <syedsamigithub@gmail.com>
2026-07-24 16:37:05 +0600

    Add files via upload

---
+++ JavaScript/Js String/object.js
@@ -0,0 +1,18 @@
+// const cap = ['kala', 'cr7', 250, 200] do not use object as an array
+
+
+const sunglass = {
+    color: 'white',
+    price: 120,
+    brand: 'gucci'
+};
+
+const phone = {
+     brand: 'Apple',
+     version: 17,
+     isThreeCamera: false,
+     secondHand: false,
+     appsInstalled: ['fb', 'chatgpt', 'youtube']
+};
+
+console.log(phone)
